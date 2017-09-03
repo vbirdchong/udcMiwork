1d4d0664ba23350a62af529960ed5685321d0075
chongchen <cc.chong.chen@gmail.com>
2017-09-03 23:06:16 +0800

    add audio manager method, then app can start, pause or stop by audio focus state

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,14 +1,22 @@
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static android.media.AudioManager.AUDIOFOCUS_GAIN_TRANSIENT;
+import static android.media.AudioManager.AUDIOFOCUS_REQUEST_FAILED;
+import static android.media.AudioManager.AUDIOFOCUS_REQUEST_GRANTED;
+import static android.media.AudioManager.STREAM_MUSIC;
 
 /**
  * Created by chong chen on 2017/8/9.
@@ -18,6 +26,8 @@ public class NumbersActivity extends AppCompatActivity {
 
     private MediaPlayer mMediaPlayer;
 
+    private AudioManager mAudioManager;
+
     private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
         @Override
         public void onCompletion(MediaPlayer mediaPlayer) {
@@ -26,10 +36,31 @@ public class NumbersActivity extends AppCompatActivity {
         }
     };
 
+    AudioManager.OnAudioFocusChangeListener mAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            if (AudioManager.AUDIOFOCUS_GAIN == focusChange) {
+                mMediaPlayer.start();
+                Log.i("NumbersActivity", "onAudioFocusChange gain");
+            } else if (AudioManager.AUDIOFOCUS_LOSS == focusChange) {
+                releaseMediaPlayer();
+                Log.i("NumbersActivity", "onAudioFocusChange release resource");
+            } else if (AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK == focusChange ||
+                        AudioManager.AUDIOFOCUS_LOSS_TRANSIENT == focusChange && mMediaPlayer != null) {
+                //mMediaPlayer.setVolume(10, 10);
+                mMediaPlayer.pause();
+                mMediaPlayer.seekTo(0);
+                Log.i("NumbersActivity", "onAudioFocusChange can duck");
+            }
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.word_list);
+
+        mAudioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         final ArrayList<Word> words = new ArrayList<Word>();
         words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
@@ -56,11 +87,18 @@ public class NumbersActivity extends AppCompatActivity {
                 releaseMediaPlayer();
 
                 Word word = words.get(i);
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioID());
-                mMediaPlayer.start();
 
-                // set the callback when audio play over
-                mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                int adReq = mAudioManager.requestAudioFocus(mAudioFocusChangeListener, STREAM_MUSIC, AUDIOFOCUS_GAIN_TRANSIENT);
+                if (adReq == AUDIOFOCUS_REQUEST_FAILED) {
+                    Log.w("NumbersActivity", "audio manager return fail");
+                } else if (adReq == AUDIOFOCUS_REQUEST_GRANTED) {
+                    Log.i("NumbersActivity", "audio manager granted");
+                    mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioID());
+                    mMediaPlayer.start();
+
+                    // set the callback when audio play over
+                    mMediaPlayer.setOnCompletionListener(mCompletionListener);
+                }
             }
         });
     }
@@ -72,6 +110,7 @@ public class NumbersActivity extends AppCompatActivity {
         if (mMediaPlayer != null) {
             mMediaPlayer.release();
             mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mAudioFocusChangeListener);
         }
     }
 }
